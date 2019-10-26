0ddba1065ccd73e95f2e54b95757703a579aaae2
andreagrossutti <57012267+andreagrossutti@users.noreply.github.com>
2019-10-26 13:20:13 +0200

    Update RootToHistograms.C
    Corrected problems in drawing histograms into Histograms class

--- root_macros/RootToHistograms.C
+++ root_macros/RootToHistograms.C
@@ -7,58 +7,46 @@ struct slimport_data_t {
 	UShort_t	samples[4096];
 };
 
-class MyBranch : public TBranch {
-	public:
-		Int_t baskets_number(){
-			return fWriteBasket;
-		} 
-};
-
-
 
 class Histograms{
 
 	public: 
 
+	//Costructor
 	Histograms(const char *name_file){
 
-		original_file = name_file;
+		original_data_file = name_file;
 		TFile* infile = new TFile(name_file);
 		TTree* intree = (TTree*)infile->Get("acq_tree_0");
 
 		for(int chan=0; chan<4; chan++){
 
 			slimport_data_t indata;
-			MyBranch *inbranch = (MyBranch*)intree->GetBranch(Form("acq_ch%d",chan));
+			TBranch *inbranch = (TBranch*)intree->GetBranch(Form("acq_ch%d",chan));
 			inbranch->SetAddress(&indata.timetag);
-			int numBins = inbranch->baskets_number();
-
-			hist [chan] = new TH1F(Form("hist_ch%d",chan),"",numBins,0,16384);
-
+			hist [chan] = new TH1F(Form("hist_ch%d",chan),"",4096,0,16384);
 
 			for (int i=0; i<inbranch->GetEntries(); i++) {
 				inbranch->GetEntry(i);
 				hist [chan]->Fill(indata.qlong);
 			}
-
 		}
 	}
 
+	//Get Histogram of channel with index "chan"
 	TH1F* GetHistogram(int chan){
 		if (chan < 0 || chan > 3)
 			return 0x0;
 		return hist[chan];
 	}
 
+	//Get path to the file with datas of the histograms
+	const char* GetDataFile(){
+		return original_data_file;
+	}
+
 	private:
 
-	const char* original_file;
-
+	const char* original_data_file;
 	TH1F* hist [4];
-
-
-
-
-
 };
-
