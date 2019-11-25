46892678cdb27b90f0caffe9a483612630643b97
AlessandroLovo <ale571.lovo@gmail.com>
2019-11-25 09:40:18 +0100

    Co

--- Timing/data/Day3/Co.C
+++ Timing/data/Day3/Co.C
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "../Coincidences.C"
 using namespace std;
 
@@ -12,23 +13,66 @@ Coincidences* Co(){
     auto channel = new vector<int>{0,1};
     auto e_min = new vector<double>{0,0};
     auto e_max = new vector<double>{DBL_MAX,DBL_MAX};
+    
 
-    for ( int i=100; i <= 1000; i++ ) {
-	e_min->at(0) = i;
-	e_min->at(1) = i;
-	C->Filter(channel,e_min)->GetHistogram(2)->Draw();
-	cout<<i<<endl;
+    Coincidences* D;
+    double fwhm = 0.;
+    double t_min = 0., t_max = 0.; //to be set
+    
+    for ( int i=1; i <= 10; i++ ) {
+	e_min->at(0) = 100*i;
+	e_min->at(1) = 100*i;
+	D = C->Filter(channel,e_min);
+    D->GetHistogram(2)->Rebin(2);
+    fwhm = FWHM(D->GetHistogram(2),t_min,t_max)
+	cout<<100*i<<endl;
     }
 
-    for ( int i=100; i <= 1000; i++ ) { 
-        e_min->at(0) = i;
-        e_min->at(1) = i;
-	e_max->at(0) = i+100;
-	e_max->at(1) = i+100;
-        C->Filter(channel,e_min,e_max)->GetHistogram(2)->Draw();
+    for ( int i=1; i <= 10; i++ ) { 
+        e_min->at(0) = 100*i;
+        e_min->at(1) = 100*i;
+	e_max->at(0) = 100*(i + 1);
+	e_max->at(1) = 100*(i + 1);
+        D = C->Filter(channel,e_min,e_max);
     }
 
 
     return C;
 }
 
+double FWHM(TH1F* h, double e_min, double e_max){
+    float centroid = 0., width_sx = 0., width_dx = 0.;
+    float max_bin_content = 0.;
+    int max_i = 0;
+    for(unsigned int i = 1; i < h->GetXaxis()->GetNbins(); i++){
+        if(h->GetXaxis()->GetBinCenter(i) < e_min)
+            continue;
+        if(h->GetXaxis()->GetBinCenter(i) > e_max)
+            break;
+        if(h->GetBinContent(i) > max_bin_content){
+            max_bin_content = h->GetBinContent(i);
+            centroid = h->GetXaxis()->GetBinCenter(i);
+            max_i = i;
+        }            
+    }
+    
+    for(unsigned int i = 0; i < max_i; i++){
+        if(h->GetXaxis()->GetBinCenter(i) < e_min)
+            continue;
+        if(h->GetBinContent(i) > max_bin_content*0.5){
+            width_sx = centroid - h->GetXaxis()->GetBinCenter(i);
+            break;
+        }
+    }
+    
+    for(unsigned int i = max_i; i < h->GetXaxis()->GetNbins(); i++){
+        if(h->GetXaxis()->GetBinCenter(i) > e_max)
+            break;
+        if(h->GetBinContent(i) < max_bin_content*0.5){
+            width_dx = h->GetXaxis()->GetBinCenter(i) - centroid;
+            break;
+        }
+    }
+    
+    return width_sx + width_dx;
+}
